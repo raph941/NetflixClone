1508c8e90dc2edac1b4dd720c51843c72535a739
raph941 <raph941@gmail.com>
2020-09-21 08:11:36 +0100

    chores: netlify deployment setup

---
+++ build/precache-manifest.e74f752ff7262cb6fea2be6442e017d7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aafff5b58f40b693de7f803a604ded07",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6d92e4a15ff5a799e2ab",
+    "url": "/static/css/main.ce426118.chunk.css"
+  },
+  {
+    "revision": "b74ebca46af63e699f49",
+    "url": "/static/js/2.2868b213.chunk.js"
+  },
+  {
+    "revision": "894f61387b383e6e2e0543fb120b4656",
+    "url": "/static/js/2.2868b213.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d92e4a15ff5a799e2ab",
+    "url": "/static/js/main.062df9d8.chunk.js"
+  },
+  {
+    "revision": "07b33697149324db54e5",
+    "url": "/static/js/runtime-main.b8a87127.js"
+  },
+  {
+    "revision": "a1f673e7df715f16dae49f4874009082",
+    "url": "/static/media/netflix.a1f673e7.png"
+  }
+]);
